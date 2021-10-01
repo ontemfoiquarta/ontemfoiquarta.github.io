b2fb6421fdad02272587688125ab6f3f22e5a913
Joao-M-Caldeira <Joao-M-Caldeira@users.noreply.github.com>
2021-10-02 00:38:19 +0100

    movebutton

---
+++ move.js
@@ -0,0 +1,17 @@
+let aboutBtn = document.getElementById('about-btn')
+let navBtns = document.getElementById('nav-buttons')
+
+function moveAbout() {
+    navBtns.style.position = 'relative'
+    navBtns.style.top = 3230 + 'px';
+}
+
+function moveDesign() {
+    navBtns.style.position = 'relative'
+    navBtns.style.top = 1150 + 'px';
+}
+
+function moveHome() {
+    navBtns.style.position = 'relative'
+    navBtns.style.top = 40 + 'px';
+}
